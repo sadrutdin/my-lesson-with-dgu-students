7210c54e162434c01badd427a6349e4bb187dfbd
Sadrutdin Zaynukov <SAZaynukov@ya.ru>
2022-10-21 22:08:52 +0300

    Добавили лог об окончании работы приложения

--- src/main/java/mvnproj/Main.java
+++ src/main/java/mvnproj/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         FileService fileService = new FileServiceImpl();
         fileService.printAll("C:\\Users\\SAZay\\IdeaProjects\\FirstMavenProject\\src\\main\\resources\\example.txt");
+        System.out.println("Конец работы приложения");
     }
 }
