56b7307a8ed004364b3c1c7d7db1b77d2883bd17
Sadrutdin Zaynukov <SAZaynukov@ya.ru>
2022-10-21 22:08:19 +0300

    Добавил лог

--- src/main/java/mvnproj/Main.java
+++ src/main/java/mvnproj/Main.java
@@ -2,6 +2,7 @@ package mvnproj;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Старт приложения");
         FileService fileService = new FileServiceImpl();
         fileService.printAll("C:\\Users\\SAZay\\IdeaProjects\\FirstMavenProject\\src\\main\\resources\\example.txt");
     }
