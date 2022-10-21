d0b68e0d15b3a6ce21811334e4ea6de18a06d840
Sadrutdin Zaynukov <SAZaynukov@ya.ru>
2022-10-21 21:58:49 +0300

    Init

---
+++ src/main/java/mvnproj/Main.java
@@ -0,0 +1,20 @@
+package mvnproj;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        File file = new File("C:\\Users\\SAZay\\IdeaProjects\\FirstMavenProject\\src\\main\\resources\\example.txt");
+        try {
+            List<String> list = FileUtils.readLines(file, StandardCharsets.UTF_8);
+            list.forEach(System.out::println);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
